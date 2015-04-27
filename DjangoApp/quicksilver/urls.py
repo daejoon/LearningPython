53035444e097e33c260962dd4a4bdfe9355a80f5
kdj <kkode1911@gmail.com>
2015-04-27 18:14:14 +0900

    정규표현식으로 Url 경로를 더 짧게 변경할수 있다.

--- DjangoApp/quicksilver/urls.py
+++ DjangoApp/quicksilver/urls.py
@@ -4,9 +4,14 @@ from quicksilver.views import HomeView, AngularTplView, NotebookView, TrashView,
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name="home"),
     url(r'^tpl/(?P<page_name>.+)$', AngularTplView.as_view(), name="angular_tpl"),
-    url(r'^notebook/$', NotebookView.as_view(), name="notebook"),
+
+    url(r'^notebook/(?P<notebook_id>-?\d+)?$', NotebookView.as_view(), name="notebookList"),
+
     url(r'^trash/$', TrashView.as_view(), name='trash'),
+
     url(r'^recentnote/$', RecentNoteView.as_view(), name='recentnote'),
-    url(r'^notelist/(?P<notebook_id>\d+)$', NoteListView.as_view(), name='notelist'),
-    url(r'^note/(?P<note_id>\d+)$', NoteView.as_view(), name='note'),
+
+    url(r'^notelist/(?P<notebook_id>-?\d+)$', NoteListView.as_view(), name='notelist'),
+
+    url(r'^note/(?P<note_id>-?\d+)?$', NoteView.as_view(), name='note'),
 ]
