224e7bd6e3722ea32bf3eebdfda88707a324219c
kdj <kkode1911@gmail.com>
2015-04-27 13:49:48 +0900

    models.Model 도 변환처리에 추가

--- DjangoApp/quicksilver/utils/ajax_util.py
+++ DjangoApp/quicksilver/utils/ajax_util.py
@@ -2,16 +2,21 @@ __author__ = 'kdj'
 from django.http import JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.query import QuerySet
+from django.db import models
 from django.forms.models import model_to_dict
+import json
 
 class AjaxResponse(JsonResponse):
-    def __init__(self, data, encoder=DjangoJSONEncoder, safe=True, **kwargs):
+    def __init__(self, data={}, encoder=DjangoJSONEncoder, safe=True, **kwargs):
         newData = {}
         try:
             if isinstance(data, QuerySet):
                 newData['data'] = [model_to_dict(item) for item in list(data)]
-            else:
+            elif isinstance(data, models.Model):
                 newData['data'] = model_to_dict(data)
+            else:
+                newData['data'] = data
+
 
             if hasattr(kwargs, 'message'):
                 newData['message'] = kwargs['message']
@@ -24,3 +29,13 @@ class AjaxResponse(JsonResponse):
             newData['status'] = True
         finally:
             super(AjaxResponse, self).__init__(newData, encoder, safe, **kwargs)
+
+class AjaxRequest(object):
+    def __init__(self, request):
+        self.request = request
+
+    def getData(self):
+        return json.loads(self.request.body)['data']
+
+    def getRequest(self):
+        return self.request
