0fa5b6c386b1ef8729d2027ccd7c26007dc6a940
kdj <kkode1911@gmail.com>
2015-05-07 21:59:17 +0900

    STATIC_ROOT는 manage.py collectstatic 명령어를 실행 할때 STATICFILES_DIRS를 포함해서 옮겨준다.

--- DjangoApp/DjangoApp/settings.py
+++ DjangoApp/DjangoApp/settings.py
@@ -102,8 +102,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
+
+# ./manage.py collectstatic 일때 사용한다.
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_URL = '/static/'
 
